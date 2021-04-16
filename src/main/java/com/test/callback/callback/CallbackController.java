a434af6974e95f5304a3fcca01527001e3f6812a
seanczw <Sean.ZW.Chong@kerrylogistics.com>
2021-04-16 19:09:28 +0800

    add Lazop SDK

--- src/main/java/com/test/callback/callback/CallbackController.java
+++ src/main/java/com/test/callback/callback/CallbackController.java
@@ -1,18 +1,32 @@
 package com.test.callback.callback;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CallbackController {
+
+    private CallbackService callbackService;
+
+    @Autowired
+    public CallbackController(CallbackService callbackService) {
+        this.callbackService = callbackService;
+    }
+
     @GetMapping("/")
     public String helloWorld() {
         return "hello world by sean!";
     }
 
     @GetMapping("/czw-token-callback")
-    public String testCallback(@RequestParam String code) {
-        return "access token callback - code: {}" + code;
+    public String testCallback(@RequestParam(required = false) String code) {
+        StringBuilder sb = new StringBuilder("SUCCESS: authorization callback.");
+        if (code != null) {
+            sb.append(" code: ");
+            sb.append(code);
+        }
+        return sb.toString();
     }
 }
